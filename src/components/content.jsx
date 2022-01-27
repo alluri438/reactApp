726918de74a72bc79e8b3c068fc4f33d111628a9
Praveen Alluri <praveen.alluri58@gmail.com>
2022-01-27 12:44:50 -0500

    change js to jsx

---
+++ src/components/content.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Fragment } from "react/cjs/react.production.min";
+
+class Content extends React.Component {
+
+    render() {
+        return (
+            <Fragment>
+                <center>
+                    <p>This is what I see </p>
+                </center>
+            </Fragment>
+        )
+    }
+}
+
+export default Content;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
+import Content from './content';
 
 const Home = () =>{
   return (
     <div>
       <Header/>
       <p>this is home page</p>
+      <Content />
       <Footer/>
     </div>
   )
